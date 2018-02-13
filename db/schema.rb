44cec15ee9a6719e7ef99a4c4b793fc25ffe97e5
rufunt <jazzoo@mail.ru>
2018-02-13 20:57:17 +0300

    Add play_id to reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180213172326) do
+ActiveRecord::Schema.define(version: 20180213175451) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 20180213172326) do
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "play_id"
   end
 
   create_table "users", force: :cascade do |t|
