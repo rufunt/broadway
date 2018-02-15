e0f30992f1d40efd5ec2678dd7b30e36f860a9a4
rufunt <jazzoo@mail.ru>
2018-02-15 23:03:03 +0300

    Add edit and update actions for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,9 @@
 class ReviewsController < ApplicationController
   before_action :find_play
+  before_action :find_review, only: [:edit, :update, :destroy]
 
 
   def new
-    
     @review = Review.new
   end
 
@@ -19,6 +19,21 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @review.update(review_params)
+      redirect_to play_path(@play)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    
+  end
+
   private
 
     def review_params
@@ -27,5 +42,9 @@ class ReviewsController < ApplicationController
 
     def find_play
       @play = Play.find(params[:play_id])
+    end
+
+    def find_review
+      @review = Review.find(params[:id])
     end
 end
